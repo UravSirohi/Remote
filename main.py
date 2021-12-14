d8e81c8a69b15ab976dacc66e20844ac087ce6d4
UravSirohi <63776019+UravSirohi@users.noreply.github.com>
2021-12-14 18:29:18 +0000

    Added Buttons

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 moving = 0
-new_password = 0
 Start = 0
 radio.set_group(144)
 radio.set_transmit_power(7)
 strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
-old_password = 399
-basic.pause(5000)
- 
- 
+
 def on_every_interval():
     global moving
     if input.button_is_pressed(Button.AB):
@@ -27,16 +23,15 @@ def on_every_interval():
         radio.send_number(5)
         strip.show_color(neopixel.colors(NeoPixelColors.PURPLE))
         moving = 1
+    elif input.pin_is_pressed(TouchPin.P0):
+        radio.send_number(6)
+        strip.show_color(neopixel.colors(NeoPixelColors.WHITE))
+        moving = 1
+    elif input.pin_is_pressed(TouchPin.P0):
+        radio.send_number(6)
+        strip.show_color(neopixel.colors(NeoPixelColors.YELLOW))
+        moving = 1
     elif moving == 1:
         radio.send_number(2)
-        moving = 1
         strip.show_color(neopixel.colors(NeoPixelColors.GREEN))
 loops.every_interval(50, on_every_interval)
- 
- 
-def on_every_interval2():
-    global new_password, old_password
-    new_password = randint(0, 100)
-    radio.send_value(str(old_password), new_password)
-    old_password = new_password
-loops.every_interval(5000, on_every_interval2)
--- main.ts
+++ main.ts
@@ -3,7 +3,8 @@ let Start = 0
 radio.setGroup(144)
 radio.setTransmitPower(7)
 let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
-loops.everyInterval(50, function () {
+loops.everyInterval(50, function on_every_interval() {
+    
     if (input.buttonIsPressed(Button.AB)) {
         if (moving == 0) {
             radio.sendNumber(2)
@@ -14,6 +15,7 @@ loops.everyInterval(50, function () {
             moving = 0
             strip.showColor(neopixel.colors(NeoPixelColors.Red))
         }
+        
     } else if (input.buttonIsPressed(Button.A)) {
         radio.sendNumber(4)
         strip.showColor(neopixel.colors(NeoPixelColors.Blue))
@@ -22,9 +24,17 @@ loops.everyInterval(50, function () {
         radio.sendNumber(5)
         strip.showColor(neopixel.colors(NeoPixelColors.Purple))
         moving = 1
+    } else if (input.pinIsPressed(TouchPin.P0)) {
+        radio.sendNumber(6)
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
+        moving = 1
+    } else if (input.pinIsPressed(TouchPin.P0)) {
+        radio.sendNumber(6)
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        moving = 1
     } else if (moving == 1) {
         radio.sendNumber(2)
-        moving = 1
         strip.showColor(neopixel.colors(NeoPixelColors.Green))
     }
+    
 })
