e2ab0c91e7fb762e90348bf47967c4dc8ccfbefe
UravSirohi <63776019+UravSirohi@users.noreply.github.com>
2021-12-17 15:53:42 +0000

    Now works with buttons

--- main.ts
+++ main.ts
@@ -3,38 +3,31 @@ let Start = 0
 radio.setGroup(144)
 radio.setTransmitPower(7)
 let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
-loops.everyInterval(50, function on_every_interval() {
-    
-    if (input.buttonIsPressed(Button.AB)) {
+loops.everyInterval(100, function () {
+    if (input.pinIsPressed(TouchPin.P0) && input.pinIsPressed(TouchPin.P1)) {
         if (moving == 0) {
             radio.sendNumber(2)
-            moving = 1
             strip.showColor(neopixel.colors(NeoPixelColors.Green))
+            moving = 1
         } else {
             radio.sendNumber(1)
-            moving = 0
             strip.showColor(neopixel.colors(NeoPixelColors.Red))
+            moving = 0
         }
-        
-    } else if (input.buttonIsPressed(Button.A)) {
+    } else if (input.pinIsPressed(TouchPin.P0)) {
         radio.sendNumber(4)
         strip.showColor(neopixel.colors(NeoPixelColors.Blue))
         moving = 1
-    } else if (input.buttonIsPressed(Button.B)) {
+    } else if (input.pinIsPressed(TouchPin.P1)) {
         radio.sendNumber(5)
         strip.showColor(neopixel.colors(NeoPixelColors.Purple))
         moving = 1
-    } else if (input.pinIsPressed(TouchPin.P0)) {
-        radio.sendNumber(6)
+    } else if (input.pinIsPressed(TouchPin.P2)) {
+        radio.sendNumber(3)
         strip.showColor(neopixel.colors(NeoPixelColors.White))
-        moving = 1
-    } else if (input.pinIsPressed(TouchPin.P0)) {
-        radio.sendNumber(6)
-        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
         moving = 1
     } else if (moving == 1) {
         radio.sendNumber(2)
         strip.showColor(neopixel.colors(NeoPixelColors.Green))
     }
-    
 })
