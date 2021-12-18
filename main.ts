d2a9bbf0cbdbe7769862d85c785c942eb119bfd4
UravSirohi <63776019+UravSirohi@users.noreply.github.com>
2021-12-18 06:30:57 +0000

    Now there are two ways to turn

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 let moving = 0
-let Start = 0
 radio.setGroup(144)
 radio.setTransmitPower(7)
 let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+let turn_type = 1
 loops.everyInterval(100, function () {
     if (input.pinIsPressed(TouchPin.P0) && input.pinIsPressed(TouchPin.P1)) {
         if (moving == 0) {
@@ -15,17 +15,31 @@ loops.everyInterval(100, function () {
             moving = 0
         }
     } else if (input.pinIsPressed(TouchPin.P0)) {
-        radio.sendNumber(4)
+        if (turn_type == 1) {
+            radio.sendNumber(4)
+        } else {
+            radio.sendNumber(6)
+        }
         strip.showColor(neopixel.colors(NeoPixelColors.Blue))
         moving = 1
     } else if (input.pinIsPressed(TouchPin.P1)) {
-        radio.sendNumber(5)
+        if (turn_type == 1) {
+            radio.sendNumber(5)
+        } else {
+            radio.sendNumber(7)
+        }
         strip.showColor(neopixel.colors(NeoPixelColors.Purple))
         moving = 1
     } else if (input.pinIsPressed(TouchPin.P2)) {
         radio.sendNumber(3)
         strip.showColor(neopixel.colors(NeoPixelColors.White))
         moving = 1
+    } else if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+        if (turn_type == 1) {
+            turn_type = 0
+        } else {
+            turn_type = 1
+        }
     } else if (moving == 1) {
         radio.sendNumber(2)
         strip.showColor(neopixel.colors(NeoPixelColors.Green))
